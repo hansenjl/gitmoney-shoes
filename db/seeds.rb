8fcedc85eca80d8ea3c7e7c5c6e5e0b5748e4486
hansenjl <jenn.leigh.hansen@gmail.com>
2020-01-21 21:16:46 -0800

    create seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Shoe.create([
+  {brand: 'Nike', color: 'black', price: 100, condition: 'new'},
+  {brand: 'Adidas', color: 'orange', price: 50, condition: 'worn'},
+  {brand: 'Nike', color: 'pink', price: 80, condition: 'muddy'},
+  {brand: 'New Balance', color: 'white', price: 70, condition: 'a few grass stains'},
+  {brand: 'Newtons', color: 'lime green', price: 180, condition: 'new'},
+  {brand: 'Allbirds', color: 'red', price: 90, condition: 'gently used'}
+])
