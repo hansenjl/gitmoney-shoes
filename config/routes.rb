8301afc4de1dde29c74359389863ae5db3799958
hansenjl <jenn.leigh.hansen@gmail.com>
2020-01-22 09:00:50 -0800

    Study group 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/shoes', to: 'shoes#index'
+  get '/shoes/most_expensive', to: 'shoes#expensive', as: 'expensive'
+  get '/shoes/:id', to: 'shoes#show', as: 'shoe'
+
+
+
+
 end
